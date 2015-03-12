355b70eb3e0efefbb14d7b84f27ec32a6723f60b
Jindra Dolezy <jindrich@dolezy.cz>
2015-03-12 21:22:57 +0100

    buildscript fix

--- build.sh
+++ build.sh
@@ -4,5 +4,9 @@ INPUT=pdfpreview
 CFLAGS=`pkg-config --cflags poppler`
 LIBS=`pkg-config --libs poppler`
 
+# note:
+#  when compiling on OSX g++ will not probably find some includes, fixing this 
+#  by adding poppler's includedir to header search path
+CFLAGS="$CFLAGS -I`pkg-config --variable=includedir poppler`";
 
 g++ -std=c++11 -Wall -pedantic $CFLAGS -o $INPUT $INPUT.cpp $LIBS && strip $INPUT
